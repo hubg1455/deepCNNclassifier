303b036a72ac1f071ac483d3507c3aad42d40ea0
Lalita <hubg1455@gmail.com>
2022-09-14 13:21:48 +0530

    logger added

---
+++ init_setup.sh
@@ -0,0 +1,9 @@
+
+echo [$(date)] : "START"
+echo [$(date)] : "Creating env with python 3.8 version  "
+conda create --prefix .env/ python=3.8 -y
+echo [$(date)] : "Activating env"
+source activate ./env
+echo [$(date)] : "installing the dev requirements"
+pip install -r requirements_dev.txt
+echo [$(date)] : "END"
